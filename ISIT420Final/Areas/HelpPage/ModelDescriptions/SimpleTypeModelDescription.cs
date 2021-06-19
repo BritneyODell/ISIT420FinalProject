5397606a5812842b662b603e1b6235202194adb4
Britney O'Dell <brodell@microsoft.com>
2021-06-19 16:11:13 -0700

    Adding working project

---
+++ ISIT420Final/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ISIT420Final.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ ISIT420Final/Controllers/PositiveEffectController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+using System.Web.Http;
+
+namespace ISIT420Final.Controllers
+{
+    public class PositiveEffectController : ApiController
+    {
+        private string countryName = "United States";
+
+        [HttpGet]
+        public IHttpActionResult GetPositiveEffect(int year, string song)
+        {
+            try
+            {
+                ISIT420FinalEntitiesConnection myCollection = new ISIT420FinalEntitiesConnection();
+
+                var songInfo = from album in myCollection.Albums
+                               from songs in myCollection.Songs
+                               where songs.SongTitle == song
+                               && songs.AlbumID == album.AlbumID
+                               select new { album.ReleaseDate, songs.Danceability };
+
+                var albumYear = DateTime.Parse(songInfo.FirstOrDefault().ReleaseDate.ToString()).Year;
+
+                // Positive Effect Of Country Health value
+                var peValue = from worldHealth in myCollection.WorldHealths
+                              where worldHealth.CountryName == countryName
+                              && worldHealth.Year == year
+                              && albumYear == worldHealth.Year
+                              select new { worldHealth.Year, worldHealth.PositiveEffect };
+
+                // PositiveEffect for U.S is above .7
+                // no need to consider this value for conditions
+                if (albumYear == peValue.FirstOrDefault().Year)
+                {
+                    if (songInfo.FirstOrDefault().Danceability >= .5)
+                    {
+                        return Ok(new { Status = "high", Year = year, PeValue = peValue.FirstOrDefault().PositiveEffect, SongTitle = song });
+                    }
+                }
+
+                // low song danceability
+                return Ok(new { Status = "slim", Year = year, PeValue = peValue.FirstOrDefault().PositiveEffect, SongTitle = song });
+            }
+            catch (Exception)
+            {
+                // No data returned from queries
+                return Ok(new { Status = "no data" });
+            }
+        }
+    }
+}
